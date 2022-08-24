743cb7dd58020771dc2a2b1bdac68c649f80c654
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-08-24 17:52:40 +0100

    seperated blog cards into three different variations

---
+++ src/components/Elements/DraftCard/DraftCard.tsx
@@ -0,0 +1,67 @@
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+
+type CardProps = {
+  authorName: string;
+  postTitle: string;
+  dateCreated: string;
+  imageUrl: string;
+  postId?: string;
+  tag: string;
+  description: string;
+  loading?: "eager" | "lazy";
+};
+
+export function DraftCard({
+  postTitle,
+  imageUrl,
+  dateCreated,
+  description,
+  tag,
+  postId,
+}: CardProps) {
+  return (
+    <div className=" border-2 border-black dark:border-white md:h-[700px] text-clip overflow-clip h-fit dark:text-white dark:bg-tertiary m-1">
+      <Link to={`/createpost/${postId}`}>
+        <div className="md:p-8 p-2 my-8 md:my-0">
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            <p className="text-lg md:text-2xl font-pilcrow mr-2 mb-2">
+              {dateCreated}
+            </p>
+            <Link
+              to={`/search/${tag}`}
+              className="border border-black font-pilcrow px-1 dark:border-white"
+            >
+              {tag}
+            </Link>
+          </motion.div>
+
+          <img
+            src={imageUrl}
+            alt={postTitle}
+            className="aspect-video object-cover border border-black my-4"
+            loading="lazy"
+          />
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          >
+            <div className="max-h-72 overflow-clip">
+              <h1 className="text-3xl md:text-4xl font-pilcrow uppercase text-ellipsis">
+                {postTitle}
+              </h1>
+              <h2 className="text-lg md:text-xl font-hind text-ellipsis">
+                {description}
+              </h2>
+            </div>
+          </motion.div>
+        </div>
+      </Link>
+    </div>
+  );
+}
--- src/features/profile/components/userownprofile/UserDrafts.tsx
+++ src/features/profile/components/userownprofile/UserDrafts.tsx
@@ -1,13 +1,12 @@
 import { collection } from "firebase/firestore";
 import { useCollectionData } from "react-firebase-hooks/firestore";
-import { Link } from "react-router-dom";
-import { BlogCard } from "src/components";
+import { DraftCard } from "src/components";
 import { useUserContext } from "src/context";
 import { draftConverter } from "src/utils";
 import { database } from "src/config/firebaseConfig";
 
 export default function UserDrafts() {
-  const {user} = useUserContext()!
+  const { user } = useUserContext()!;
   const ref = collection(database, "drafts").withConverter(draftConverter);
   const [data] = useCollectionData(ref);
   const userPosts = data?.filter((doc) => doc.author.id === user?.uid);
@@ -18,16 +17,14 @@ export default function UserDrafts() {
       {userPosts && userPosts.length > 0 ? (
         <article className="flex flex-wrap">
           {userPosts!.map((doc, index) => (
-            <Link to={`/createpost/${doc.id}`} key={doc.id} className="w-fit md:w-6/12 xl:w-3/12 lg:w-4/12">
-              <BlogCard
-                authorName={doc.author.name}
-                tag={doc.tag}
-                description={doc.description}
-                dateCreated={doc.dateCreated}
-                imageUrl={doc.imageDownloadUrl}
-                postTitle={`Draft ${index + 1}`}
-              />
-            </Link>
+            <DraftCard
+              authorName={doc.author.name}
+              tag={doc.tag}
+              description={doc.description}
+              dateCreated={doc.dateCreated}
+              imageUrl={doc.imageDownloadUrl}
+              postTitle={`Draft ${index + 1}`}
+            />
           ))}
         </article>
       ) : (
