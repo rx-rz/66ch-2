930a6e672b0f68b47df7b32976b744fc7bf149b3
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-08-26 20:38:28 +0100

    wrote integration tests for register and login

---
+++ src/features/auth/components/__tests__/Login.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import AppProvider from "src/provider/app";
+import { LoginForm } from "../LoginForm";
+
+const Login = (
+  <AppProvider>
+    <LoginForm />
+  </AppProvider>
+);
+
+test("renders learn react link", async () => {
+  render(Login);
+  userEvent.type(screen.getByLabelText(/email/i), "adeleye@gmail.com")
+  userEvent.type(screen.getByLabelText(/password/i), "adeleye")
+  const loginButton = screen.getByRole("button", { name: /log in/i });
+  userEvent.click(loginButton)
+  expect(window.location.pathname).toEqual("/")
+});
+
+
+test("register button reroutes to register page", () => {
+  render(Login);
+  const registerButton = screen.getByRole("button", { name: /register/i });
+  userEvent.click(registerButton);
+  expect(window.location.pathname).toEqual("/auth/register");
+});
---
+++ src/features/auth/components/__tests__/Register.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import { RegisterForm } from "../RegisterForm";
+import AppProvider from "src/provider/app";
+import userEvent from "@testing-library/user-event";
+
+const Register = (
+  <AppProvider>
+    <RegisterForm />
+  </AppProvider>
+);
+
+test("user should be registered",  () => {
+  render(Register);
+  userEvent.type(screen.getByLabelText(/first name/i), "Akinlolu");
+  userEvent.type(screen.getByLabelText(/last name/i), "Akinlolu");
+  userEvent.type(screen.getByLabelText(/password/i), "akinlolu");
+  userEvent.type(screen.getByLabelText(/email/i), "akinlolu@gmail.com");
+  const registerButton = screen.getByRole("button", { name: /register/i });
+  userEvent.click(registerButton);
+  expect(window.location.pathname).toEqual("/");
+});
+
+test("login button reroutes to login page",  () => {
+  render(Register);
+
+  const loginButton = screen.getByRole("button", { name: /log in/i });
+  userEvent.click(loginButton);
+  expect(window.location.pathname).toEqual("/auth/login");
+});
