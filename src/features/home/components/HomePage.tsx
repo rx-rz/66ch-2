df73d523deeb077d68f8da19e69b9aa66decae04
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-08-27 20:35:06 +0100

    changed default posts

--- src/features/home/components/HomePage.tsx
+++ src/features/home/components/HomePage.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 import { usePostContext } from "src/context/postContext";
 export default function HomePage() {
   const { data } = usePostContext()!;
-  const blogPostOne = data && data[2];
-  const blogPostTwo = data && data[3];
+  const blogPostOne = data && data[5];
+  const blogPostTwo = data && data[6];
   
   return (
     <div className="w-full border-2 border-t-0 border-tertiary text-white">
