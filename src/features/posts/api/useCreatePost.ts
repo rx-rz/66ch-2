a745f94cce7d73890f2ae1f13068fdb051ae8686
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2023-01-12 14:30:09 +0100

    added comments to useCreatePost section

--- src/features/posts/api/useCreatePost.ts
+++ src/features/posts/api/useCreatePost.ts
@@ -88,16 +88,22 @@ const postsRef = collection(database, "posts");
 const draftsRef = collection(database, "drafts");
 
 export const useCreatePost = () => {
+  // Get user from context
   const { user } = useUserContext()!;
 
+  // Get post ID from URL params, set default value
   const { id = "3279ghdga!&@E&*^#&%$^!" } = useParams();
+  // Initialize state for pending, draft ID, and editor content
   const [pending, setPending] = useState(false);
   const [draftId, setDraftId] = useState<string | null>(null);
   const [editorContent, setEditorContent] = useState("");
 
+  // Reference to specific draft
   const draftRef = doc(database, "drafts", id!);
+  // Fetch draft data
   const [draft] = useDocumentData(draftRef);
 
+  // Function to update editor content state
   const changeEditorContent = (editorContent: string) => {
     setEditorContent(editorContent);
   };
@@ -109,13 +115,17 @@ export const useCreatePost = () => {
     description: string | undefined
   ) => {
     console.log(data.postTitle, editorContent, user, tag);
+    // Check if post title and editor content are present
     if (data.postTitle && editorContent) {
+      // Check if image, tag and description are present
       if (
         (imageUrl ?? draft?.imageDownloadUrl) &&
         (tag ?? draft?.tag) &&
         (description ?? draft?.description)
       ) {
+        // Update pending state
         setPending(true);
+        // Add new post to "posts" collection
         await addDoc(postsRef, {
           postTitle: data.postTitle,
           postContent: editorContent,
@@ -128,17 +138,22 @@ export const useCreatePost = () => {
           isChecked: user && user.role === "admin" ? true : false,
         });
 
+        // Delete the draft
         if (draft) {
           await deleteDoc(doc(database, "drafts", id!));
         }
-
+        // Update pending state
         setPending(false);
+        // Toast message
         postContentToast();
+        //Redirect to homepage
         window.location.pathname = "/";
       } else {
+        // Toast message
         errorToast();
       }
     } else {
+      // Toast message
       postContentToast();
     }
   };
@@ -150,8 +165,10 @@ export const useCreatePost = () => {
     draft: Partial<Blog> | undefined,
     postTitle?: string
   ) => {
+    //Check if draft is not present
     if (!draft) {
       if (!draftId) {
+        //Add new draft to "drafts" collection
         await addDoc(draftsRef, {
           postContent: editorContent ?? "",
           postTitle: postTitle,
@@ -161,8 +178,10 @@ export const useCreatePost = () => {
           tag: tag ?? "",
           description: description ?? "",
         }).then((docRef) => setDraftId(docRef.id));
+        // Toast message
         draftToast();
       } else {
+        // Update draft in "drafts" collection
         await updateDoc(doc(database, "drafts", draftId!), {
           postContent: editorContent ?? "",
           imageDownloadUrl: imageUrl ?? "",
@@ -172,9 +191,11 @@ export const useCreatePost = () => {
           tag: tag ?? "",
           description: description ?? "",
         });
+        // Toast message
         draftToast();
       }
     } else {
+      // Update draft in "drafts" collection
       await updateDoc(doc(database, "drafts", id!), {
         postContent: editorContent ?? "",
         postTitle: postTitle,
@@ -184,6 +205,7 @@ export const useCreatePost = () => {
         tag: tag ?? "",
         description: description ?? "",
       });
+      // Toast message
       draftToast();
     }
   };
