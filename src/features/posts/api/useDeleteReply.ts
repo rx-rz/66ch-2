855e7232bfdc7d80af7c556c76211b335158336d
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2023-01-12 14:34:40 +0100

    added comments to useDeleteReply API page

--- src/features/posts/api/useDeleteReply.ts
+++ src/features/posts/api/useDeleteReply.ts
@@ -4,9 +4,13 @@ import { useState } from "react";
 import { database } from "src/config/firebaseConfig";
 
 export const useDeleteReply = () => {
+  // Initialize state for reply delete error
   const [replyDeleteError, setReplyDeleteError] = useState("");
+
   const handleReplyDelete = async (id: string) => {
+    // Delete reply
     await deleteDoc(doc(database, "replies", id)).then((err: any) => {
+      // check if there's an error
       if (err instanceof Error || FirebaseError) {
         setReplyDeleteError(err.message);
       }
