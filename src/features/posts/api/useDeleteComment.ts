1d0bcd249c34b60802bcb71787a76ac82ef9d094
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2023-01-12 14:33:28 +0100

    added comments to useDeleteComment API page

--- src/features/posts/api/useDeleteComment.ts
+++ src/features/posts/api/useDeleteComment.ts
@@ -4,14 +4,18 @@ import { useState } from "react";
 import { database } from "src/config/firebaseConfig";
 
 export const useDeleteComment = () => {
+  // Initialize state for comment delete error
   const [commentDeleteError, setCommentDeleteError] = useState("");
+
   const handleCommentDelete = async (id: string) => {
+    //Delete comment
     await deleteDoc(doc(database, "comments", id)).then((err: any) => {
+      // check if there's an error
       if (err instanceof Error || FirebaseError) {
         setCommentDeleteError(err.message);
       }
     });
   };
 
-  return {commentDeleteError, handleCommentDelete}
+  return { commentDeleteError, handleCommentDelete };
 };
